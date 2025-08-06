c36ec27a48f12a7bf913a775c48d6a963c3bf698
Piyush <piyushsinghthakur128@gmail.com>
2025-08-06 23:57:21 +0530

    Rhombus Pattern and Butterfly Pattern

---
+++ src/com/ok/javainonevideo/Patterns/ButterflyPattern.java
@@ -0,0 +1,41 @@
+package com.ok.javainonevideo.Patterns;
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        int n = 4;
+        //Upper part
+        for (int i=1; i<=n; i++){
+            //first part
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            //spaces
+            int spaces = 2*(n-i);
+            for (int j=1; j<=spaces; j++){
+                System.out.print("  ");
+            }
+            //2nd part
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        //Lower part
+        for (int i=n; i>=1; i--){
+            //first part
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            //spaces
+            int spaces = 2*(n-i);
+            for (int j=1; j<=spaces; j++){
+                System.out.print("  ");
+            }
+            //2nd part
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/com/ok/javainonevideo/Patterns/SolidRhombus.java
@@ -0,0 +1,19 @@
+package com.ok.javainonevideo.Patterns;
+
+public class SolidRhombus {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i=1; i<=n; i++){
+            //spaces
+            int spaces = n-i;
+            for (int j=1; j<=spaces; j++){
+                System.out.print("  ");
+            }
+            //stars
+            for (int j=1; j<=5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
