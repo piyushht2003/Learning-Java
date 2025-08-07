a97ba13eb83abc893dcd913dbb0e224342042b69
Piyush <piyushsinghthakur128@gmail.com>
2025-08-08 00:13:27 +0530

    Function to get factorial values

--- src/com/ok/javainonevideo/Functions/Function.java
+++ src/com/ok/javainonevideo/Functions/Function.java
@@ -3,16 +3,26 @@ package com.ok.javainonevideo.Functions;
 import java.util.Scanner;
 
 public class Function {
-    public static int calculateSum(int a, int b){
-        int sum = a+b;
-        return sum;
+//    public static int calculateSum(int a, int b){
+//        return a+b;
+//    }
+
+    public static void printFactorial(int n) {
+        if (n<0){
+            System.out.println("Invalid input");
+            return;
+        }
+        int factorial = 1;
+        for (int i=n; i>=1; i--){
+            factorial = factorial * i;
+        }
+        System.out.println("The factorial of the number is : "+factorial);
+        return;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        int sum = calculateSum(a,b);
-        System.out.println("Sum of two number is : "+sum);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        printFactorial(n);
     }
 }
